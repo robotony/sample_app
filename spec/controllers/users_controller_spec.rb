c3a7f385f291539884ba2f9f402a7c7dd20085da
Tony Robinson <trobinson@Tony-Robinsons-MacBook-Pro.local>
2011-02-25 16:21:38 -0800

    finishing up sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -95,6 +95,7 @@ describe UsersController do
         post :create, :user => @attr
         response.should render_template('new')
       end
+      
     end
     
 		describe "success" do
@@ -118,6 +119,11 @@ describe UsersController do
 			it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
+      end
+
+			it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
       
     end
